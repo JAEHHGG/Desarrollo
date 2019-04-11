93a1ef8c540558497645f72f25846e883608a717
jaehhgg <jhernan83075@universidadean.edu.co>
2019-04-11 07:37:08 -0500

    Ejercicio hecho por el profesor

---
+++ PHP/Ejercicio profesor/respuesta.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Respuesta</title>
+</head>
+<body>
+    <?php
+    $pass = [
+    "jose" => "cordoba",
+    "manuel" => "castillo",
+    "krusty" => "ean",
+    ];
+    if (isset($pass[$_GET['usser']])){
+        if ($pass[$_GET['usser']] == $_GET['pass']){
+            echo 'Bienvenido';
+        }else{
+            echo 'Contraseña incorrecta';
+        }
+    }else{
+        echo 'Usuario no encontrado';
+    }
+    ?>
+</body>
+</html>
---
+++ PHP/Ejercicio/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Formulario Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="modal-dialog text-center">
+        <div class="col-sm-8 main-section">
+            <div class="modal-content">
+                <div class="col-12 user-img">
+                    <img src="img/cambiarlogo.png" alt="Logo Prácticas Colombia">
+
+                    <form class="col-12" onsubmit="return validar();">
+                        <div class="form-group">
+                            <input id="correo" type="email" class="form-control" placeholder="Correo electrónico" required>
+                        </div>
+                        <div class="form-group">
+                            <input id="contraseña" type="password" class="form-control" placeholder="Contraseña" required>
+                        </div>
+                        <button type="submit" class="btn"><i class="fas "></i>ENVIAR</button>
+                    </form>
+                    <div class="col-12 forgot">
+                        <a href="#">Registrarse</a>
+                    </div>
+                </div>
+            </div> <!--Final del Modal Content-->
+        </div>
+    </div>
+<script src="main.js" type="text/javascript"></script>
+</body>
+</html>
