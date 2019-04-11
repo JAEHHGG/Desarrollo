bbf9d3b00a78a2d04de2538f37069862185442ca
jaehhgg <jhernan83075@universidadean.edu.co>
2019-04-11 18:07:25 -0500

    mas pruebas

---
+++ PHP/Ejemplo/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <form action="val.php" method="get">
+        <input type="text" name="usuario" id="usuario" placeholder="Usuario">
+        <input type="password" name="contraseña" id="contraseña" placeholder="Contraseña">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ PHP/Ejemplo/val.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>val</title>
+</head>
+<body>
+    <?php
+    $contraseña = [
+        "Julio" => "Hernández",
+        "Juan" => "Perez",
+        "Natalia" => "Morales",
+        "Sofia" => "Mora",
+    ];
+
+    if (isset($contraseña[$_GET['usuario']])) {
+        if ($contraseña[$_GET['usuario']] == $_GET['pass']){
+            echo 'Hola, Bienvenido';
+        } else {
+            echo 'Verificar Usuario y/o contraseña'
+        }
+    } else {
+        echo 'Usuario incorrecto'
+    }
+    ?>
+</body>
+</html>
+
